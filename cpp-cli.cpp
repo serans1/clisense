9b23523b807a4e63c5833533d24c03931e2071cb
perc <eran.shlomo@intel.com>
2016-09-18 17:40:38 +0300

    Auto commit

--- cpp-cli.cpp
+++ cpp-cli.cpp
@@ -65,8 +65,8 @@ int main() try
     printf("CliSense 0.0.1\n");
 	/** Define and parse the program options 
      */ 
-    /*namespace po = boost::program_options; 
-    po::options_description desc("Options"); 
+    namespace po = boost::program_options; 
+    /*po::options_description desc("Options"); 
     desc.add_options() 
       ("help", "Print help messages") 
       ("add", "additional options") 
